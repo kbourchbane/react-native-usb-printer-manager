61ac92fae7afb285bd50bf2fa903bb2d4a718a88
kbourchbane <kamal.bourchbane@mts-system.com>
2024-10-02 15:06:14 +0100

    add get devices list function

--- android/src/main/java/com/usbprintermanager/UsbPrinterManagerModule.kt
+++ android/src/main/java/com/usbprintermanager/UsbPrinterManagerModule.kt
@@ -4,6 +4,8 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
+import android.hardware.usb.UsbManager
+import java.util.HashMap
 
 class UsbPrinterManagerModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
@@ -19,7 +21,51 @@ class UsbPrinterManagerModule(reactContext: ReactApplicationContext) :
     promise.resolve(a * b)
   }
 
+  @ReactMethod
+  fun getDevicesList(promise: Promise) {
+
+    // create buttons in a loop
+    val manager: UsbManager = getSystemService(Context.USB_SERVICE) as UsbManager
+    val deviceList: HashMap<String, UsbDevice> = manager.getDeviceList()
+
+    val gson = Gson()
+    val response: String = gson.toJson(deviceList)
+    val returnMap: WritableMap = this.util.convertJsonToMap(JSONObject(response))
+
+    promise.resolve(returnMap)
+  }
+
   companion object {
     const val NAME = "UsbPrinterManager"
   }
+
+  @kotlin.Throws(JSONException::class)
+  fun convertJsonToMap(jsonObject: JSONObject): WritableMap? {
+    val map: WritableMap = WritableNativeMap()
+    val iterator: Iterator<String> = jsonObject.keys()
+    while (iterator.hasNext()) {
+      val key = iterator.next()
+      val value: Object = jsonObject.get(key)
+      if (value is JSONObject) {
+        map.putMap(key, convertJsonToMap(value as JSONObject))
+      } else if (value is JSONArray) {
+        map.putArray(key, convertJsonToArray(value as JSONArray))
+        if ("option_values".equals(key)) {
+          map.putArray("options", convertJsonToArray(value as JSONArray))
+        }
+      } else if (value is Boolean) {
+        map.putBoolean(key, value as Boolean)
+      } else if (value is Integer) {
+        map.putInt(key, value as Integer)
+      } else if (value is Double) {
+        map.putDouble(key, value as Double)
+      } else if (value is String) {
+        map.putString(key, value as String)
+      } else {
+        map.putString(key, value.toString())
+      }
+    }
+    return map
+  }
+
 }
